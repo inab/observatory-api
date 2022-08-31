4b9e91b98f33b4b88f7316d0b159260b67cff697
Eva Martin del Pico <eva.mdpico@gmail.com>
2022-08-31 14:06:39 +0200

    Refactor app.py and new endpoints

--- app.py
+++ app.py
@@ -6,43 +6,21 @@ from pymongo import MongoClient
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS,cross_origin
 
+import db_query
+
 # connecting to db
 config = configparser.ConfigParser()
-config.read('config_db.ini')
+config.read('dev_config_db.ini')
 DBHOST = config['MONGO_DETAILS']['DBHOST']
 DBPORT = config['MONGO_DETAILS']['DBPORT']
 DATABASE = config['MONGO_DETAILS']['DATABASE']
 TOOLS = config['MONGO_DETAILS']['TOOLS']
-ALAMBIQUE = config['MONGO_DETAILS']['ALAMBIQUE']
 STATS = config['MONGO_DETAILS']['STATS']
 
 connection = MongoClient(DBHOST, int(DBPORT))
-collection = connection[DATABASE][TOOLS]
+tools_collection = connection[DATABASE][TOOLS]
 stats = connection[DATABASE][STATS]
 
-def latest_count():
-    record = list(stats.find({
-        "variable":"tools_counts_per_source"
-        }).sort("version",-1).limit(1))
-    return(record)
-
-def version_count(version):
-    try:
-        record = list(stats.find({
-            "variable":"tools_counts_per_source",
-            "version":version}))
-    except Exception:
-        return(Exception)
-    else:
-        return(record)
-
-def total_count():
-    record = list(stats.find({"variable":"tools_count"}).sort("version",-1).limit(1))
-    return(record)
-
-def FAIR_scores():
-    record = list(stats.find({"variable":"FAIR_scores"}).sort("version",-1).limit(1))
-    return(record)
 
         
 ## Init app
@@ -58,6 +36,7 @@ def process_request(action, parameters):
         data = action(parameters)
         
     except Exception as err:
+        print(f'Error here {err}')
         resp = make_response(err, 400)
         print(err)
     else:
@@ -66,72 +45,169 @@ def process_request(action, parameters):
     finally:
         return resp
 
-def action_fair_scores(parameters):
-    try:
-        docs=FAIR_scores()
-    except Exception as err:
-        return(str(err))
-    else:
-        [entry.pop('_id') for entry in docs]
-    return(docs)
 
-def action_count_total(parameters):
-    try:
-        docs=total_count()
-    except Exception as err:
-        return(str(err))
-    else:
-        [entry.pop('_id') for entry in docs]
-    return(docs)
-
-
-def action_counts_source(parameters):
-    try:
-        if request.args.get('version'):
-            if request.args.get('version') == 'latest':
-                docs = latest_count()
-            else:
-                version = request.args.get('version'+1)
-                docs = version_count(version)
+def query(func):
+    '''
+    Error handling of query execution and removal of _id field
+    '''
+    def wrapper(*args, **kwargs):
+        try:
+            docs = func(*args, **kwargs)
+        except Exception as err:
+            raise
+            return(str(err))
         else:
-            docs = latest_count()
+            [entry.pop('_id') for entry in docs]
+        
+        if len(docs) == 0:
+            return('No results found')
+        else:
+            return(docs)
+        
+    return wrapper
 
+
+def clean_quotations(string):
+    string = string.replace('"', '')
+    string = string.replace("'", '')
+    return string
+
+def prep_parameters(parameters):
+    '''
+    If version not stated, assign `latest` version
+    If collection not stated, assign `tools` collection
+    '''
+    try:
+        if parameters.get('version'):
+            version = clean_quotations(parameters.get('version'))
+        else:
+            version = 'latest'
+        
+        if parameters.get('collection'):
+            collection = clean_quotations(parameters.get('collection'))
+        else:
+            collection = 'tools'
+        
     except Exception as err:
-        return(('An error occurred while trying to fetch your queried data.'
-                ' Please, check the version in your query and retry.'
-                f' Error message: {str(err)}'))
+        return(('An error occurred while trying to read query parameters.'
+            ' Please, check version and collection in your query and retry.'
+            f' Error message: {str(err)}'))
+
     else:
-        print(docs)
-        [entry.pop('_id') for entry in docs]
-    return(docs)
+        return collection, version
+    
+@query
+def make_query(variable_name, parameters):
+    collection, version = prep_parameters(parameters)
+    if version == 'latest':
+        record = list(stats.find({
+            "variable":variable_name,
+            "collection":collection,
+            }).sort("version",-1).limit(1))
+    else:
+        record = list(stats.find({
+            "variable":variable_name,
+            "collection":collection,
+            "version":version
+            }))
+    
+    return record
 
+#####
+## Requests regarding docs in `stats` collection
+####
 
+# Number of Tools per source
 @app.route('/stats/tools/count_per_source')
 @cross_origin(origin='*',headers=['Content-Type'])
 def counts_per_source():
-    resp = process_request(action_counts_source, request.args)
+    resp =  make_query('tools_counts_per_source', request.args)
+    #resp = process_request(counts_source_query, request.args)
     return(resp)
 
-
+# Total Number of tools
 @app.route('/stats/tools/count_total')
 @cross_origin(origin='*',headers=['Content-Type'])
 def count_total():
-    resp = process_request(action_count_total, request.args)
+    resp = make_query('tools_count', request.args)
     return(resp)
 
+
+# licenses-sunburst - licenses_summary_sunburst
+@app.route('/stats/tools/licenses_summary_sunburst')
+@cross_origin(origin='*',headers=['Content-Type'])
+def licenses_summary_sunburst():
+    resp = make_query('licenses_summary_sunburst', request.args)
+    return(resp)
+
+# licenses-bar - licenses_open_source
+@app.route('/stats/tools/licenses_open_source')
+@cross_origin(origin='*',headers=['Content-Type'])
+def licenses_open_source():
+    resp = make_query('licenses_open_source', request.args)
+    return(resp)
+
+# semantic versioning - semantic_versioning
+@app.route('/stats/tools/semantic_versioning')
+@cross_origin(origin='*',headers=['Content-Type'])
+def semantic_versioning():
+    resp = make_query('semantic_versioning', request.args)
+    return(resp)
+
+
+# version control yes/no counts - version_control_count
+@app.route('/stats/tools/version_control_count')
+@cross_origin(origin='*',headers=['Content-Type'])
+def version_control_count():
+    resp = make_query('version_control_count', request.args)
+    return(resp)
+
+# version control repositories counts - version_control_repositories
+@app.route('/stats/tools/version_control_repositories')
+@cross_origin(origin='*',headers=['Content-Type'])
+def version_control_repositories():
+    resp = make_query('version_control_repositories', request.args)
+    return(resp)
+
+# types tools - types_count
+@app.route('/stats/tools/types_count')
+@cross_origin(origin='*',headers=['Content-Type'])
+def types_count():
+    resp = make_query('types_count', request.args)
+    return(resp)
+
+# features cummulative - features_cummulative
+@app.route('/stats/tools/features_cummulative')
+@cross_origin(origin='*',headers=['Content-Type'])
+def features_cummulative():
+    resp = make_query('features_cummulative', request.args)
+    return(resp)
+
+# features xy - distribution_features
+@app.route('/stats/tools/distribution_features')
+@cross_origin(origin='*',headers=['Content-Type'])
+def distribution_features():
+    resp = make_query('distribution_features', request.args)
+    return(resp)
+
+
+# FAIR scores
 @app.route('/stats/tools/fair_scores')
 @cross_origin(origin='*',headers=['Content-Type'])
 def fair_scores():
-    resp = process_request(action_fair_scores, request.args)
+    resp = process_request(action_fair_scores_query, request.args)
     return(resp)
 
-
+#####
+## Requests regarding docs in `tools_collection` collection
+####
+# Retrieve all tools
 @app.route('/tools', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def tools():
     try:
         tools = []
-        [tools.append(entry) for entry in collection.find({})]
+        [tools.append(entry) for entry in tools_collection.find({})]
         [entry.pop('_id') for entry in tools]
         data = {
             'tools' : tools
@@ -146,12 +222,14 @@ def tools():
     finally:
         return resp
 
+# Retrieve description of a tool by its name
+# name as parameter
 @app.route('/tool/description', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def description():
     try:
         tool_name = request.args.get('name')
-        entry = collection.find_one({'name' : tool_name})
+        entry = tools_collection.find_one({'name' : tool_name})
         data = { 
             'name' : tool_name,
             'type': entry['type'],
