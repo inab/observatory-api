53f84d0c2b97747283eb7621924db2900c5970ce
emarti2 <eva.martin@bsc.es>
2023-06-01 13:14:53 +0200

    added files

---
+++ prepareVocabularies.py
@@ -0,0 +1,31 @@
+from EDAM_forFE import EDAMDict 
+
+def prepareEDAM():
+    EDAMVocabularyItems = {
+        'format': [],
+        'datatype': [],
+        'operation': [],
+        'topic': [],
+    }
+
+    for key,value in EDAMDict.items():
+        # if this is an EDAM term
+        if 'http://edamontology.org' in key:
+
+            # Find out type of EDAM term it is
+            if 'format' in key:
+                EDAMVocabularyItems['format'].append(value) 
+            elif 'data' in key:
+                EDAMVocabularyItems['datatype'].append(value)
+            elif 'operation' in key:
+                EDAMVocabularyItems['operation'].append(value)
+            elif 'topic' in key:
+                EDAMVocabularyItems['topic'].append(value)
+            else:
+                print(f'Error: EDAM term not recognised: key: {key}, value: {value}')
+    
+    return EDAMVocabularyItems
+
+if __name__ == "__main__":
+    EDAMVocabularyItems = prepareEDAM()            
+    print(EDAMVocabularyItems)
