b19d10fc8ca18990f97bce028c3ad2566b6a3f49
Eva Martin del Pico <eva.mdpico@gmail.com>
2025-09-08 12:38:20 +0200

    fix typo in feedback

--- app/helpers/indicators_feedback.py
+++ app/helpers/indicators_feedback.py
@@ -31,7 +31,7 @@ def get_feedback(result):
     for indicator in feedback_per_indicator.keys():
         if indicator[0] in ['F', 'A', 'I', 'R']:
             if  result.get(indicator) == True:
-                feedback[indicator[0]]['strenghts'].append(feedback_per_indicator[indicator]['strengths'])
+                feedback[indicator[0]]['strengths'].append(feedback_per_indicator[indicator]['strengths'])
             elif result.get(indicator) == False:
                 feedback[indicator[0]]['improvements'].append(feedback_per_indicator[indicator]['improvements'])
 
